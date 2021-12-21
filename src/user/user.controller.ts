85cf55fba340b2472d7caa1c060f53e1ac9aab4a
zattaoui <03zaki95@gmail.com>
2021-12-21 16:07:33 +0100

    create user controler with findAll & findOne request

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @Get('/')
+  findAll() {
+    return 'This action returns all users';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} user`;
+  }
+}
